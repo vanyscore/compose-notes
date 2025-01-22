cb9b422d162193e0931054addc261f6ccd774326
vanyscore <vanyscore@gmail.com>
2025-01-22 15:13:04 +0500

    Clear focus on text fields edit in NoteScreen.

---
+++ app/src/main/java/com/vanyscore/app/ui/ModifierExtension.kt
@@ -0,0 +1,15 @@
+package com.vanyscore.app.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.noIndicationClickable(onClick: () -> Unit): Modifier = composed {
+    Modifier.clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+        onClick = onClick
+    )
+}
--- app/src/main/java/com/vanyscore/notes/NoteScreen.kt
+++ app/src/main/java/com/vanyscore/notes/NoteScreen.kt
@@ -1,10 +1,14 @@
 package com.vanyscore.notes
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.BottomAppBar
@@ -23,14 +27,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vanyscore.app.AppState
 import com.vanyscore.app.ui.AttachmentsControl
+import com.vanyscore.app.ui.noIndicationClickable
 import com.vanyscore.notes.viewmodel.NoteViewModel
 import com.vanyscore.tasks.R
 
@@ -55,7 +63,11 @@ fun NoteScreen(
     if (state.value.canClose) {
         navController?.navigateUp()
     }
+    val focusManager = LocalFocusManager.current
     return Scaffold(
+        modifier = Modifier.noIndicationClickable {
+            focusManager.clearFocus()
+        },
         topBar = {
             TopAppBar(
                 title = {
@@ -115,7 +127,16 @@ fun NoteScreen(
             item {
                 TextField(
                     note.title,
+                    singleLine = true,
                     modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            focusManager.clearFocus()
+                        }
+                    ),
                     label = {
                         Text(stringResource(R.string.title))
                     },
